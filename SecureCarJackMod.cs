71b6f96aaa3a81d8f863507c4c8781ddc8ba2a0f
Tommo J. Phillips <tommojphillips@gmail.com>
2019-03-07 19:53:17 +1100

    Add project files.

---
+++ SecureCarJackMod.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using HutongGames.PlayMaker;
+using MSCLoader;
+using ModApi.Attachable;
+
+namespace SecureCarJack
+{
+    public class SecureCarJackMod : Mod
+    {
+        // Written, 07.03.2019
+
+        public override string ID => "SecureCarJack"; //Your mod ID (unique)
+        public override string Name => "Secure Car Jack"; //You mod name
+        public override string Author => "tommojphillips"; //Your Username
+        public override string Version => "0.1"; //Version
+
+
+        private GameObject carJackItemx;
+        private bool carJackOpen
+        {
+            get
+            {
+                return PlayMakerFSM.FindFsmOnGameObject(this.carJackItemx, "Fold").FsmVariables.FindFsmBool("Open").Value;
+            }
+        }
+
+        public override void OnLoad()
+        {
+            // Written, 07.03.2019
+            // Called once, when mod is loading after game is fully loaded
+
+            carJackItemx = GameObject.Find("car jack(itemx)");                       
+        }
+    }
+}
