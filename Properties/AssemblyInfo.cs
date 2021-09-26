be4762a5300a43bcf7592c5d81ebb12bb2667c19
Tommo J. Phillips <tommojphillips@gmail.com>
2021-09-26 16:06:39 +1000

    updated mod to v1.0.0 mod now supports modapi v0.1.4.1-alpha

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("A mod for My Summer Car that implements the feature to secure the car jack into the trunk of the car.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Tommo J. Productions")]
-[assembly: AssemblyProduct("Secure Car Jack")]
-[assembly: AssemblyCopyright("Copyright © Tommo J. Productions 2019")]
+[assembly: AssemblyProduct("Secure Car Jack v1.0.0")]
+[assembly: AssemblyCopyright("Copyright © Tommo J. Productions 2021")]
 [assembly: AssemblyTrademark("Azine")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- SecureCarJackMod.cs
+++ SecureCarJackMod.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
-using HutongGames.PlayMaker;
 using MSCLoader;
-using ModApi.Attachable;
+using TommoJProductions.ModApi.Attachable;
+using static TommoJProductions.ModApi.Attachable.Part;
 
 namespace SecureCarJack
 {
@@ -9,57 +9,55 @@ namespace SecureCarJack
     {
         // Written, 07.03.2019
 
-        public override string ID => "SecureCarJack"; //Your mod ID (unique)
-        public override string Name => "Secure Car Jack"; //You mod name
-        public override string Author => "tommojphillips"; //Your Username
-        public override string Version => "0.1"; //Version
+        public const string FILE_NAME = "SecureCarJack.txt";
+
+        public override string ID => "SecureCarJack";
+        public override string Name => "Secure Car Jack"; 
+        public override string Author => "tommojphillips"; 
+        public override string Version => "1.0.0";
         
-        /// <summary>
-        /// Represents the car jack part.
-        /// </summary>
-        private CarJackPart carJackPart;
-        /// <summary>
-        /// Represents the save file name.
-        /// </summary>
-        private const string fileName = "SecureCarJack.txt";
-        /// <summary>
-        /// Loads save data.
-        /// </summary>
-        private PartSaveInfo loadData()
-        {
-            // Written, 12.10.2018
+        private Part carJackPart;
 
-            try
-            {
-                return SaveLoad.DeserializeSaveFile<PartSaveInfo>(this, fileName);
-            }
-            catch (System.NullReferenceException)
-            {
-                // no save file exists.. // setting/loading default save data.
+        private PlayMakerFSM foldFsm;
 
-                return this.carJackPart.defaultPartSaveInfo;
-            }
-        }
         /// <summary>
         /// Occurs on game start.
         /// </summary>
         public override void OnLoad()
         {
-            // Written, 07.03.2019
-            // Called once, when mod is loading after game is fully loaded
+            // Written, 07.03.2019 | Modified, 25.09.2021
 
-            GameObject parent = GameObject.Find("SATSUMA(557kg, 248)"); // gameobject that will be attached to car jack itemx, in this case the satsuma!.
-            // Creating trigger for car jack itemx. and assigning the local location and scale of the trigger related to the parent. (in this case the satsuma).
-            Trigger trigger = new Trigger("carJackTrigger", parent, new Vector3(-0.447f, 0, -1.48f), Quaternion.Euler(0, 285, 0), new Vector3(0.2f, 0.2f, 0.24f), false);
-            // Creating a new instance of the carjackpart
-            this.carJackPart = new CarJackPart(this.loadData(), // Loading saved or default data.
-                GameObject.Find("car jack(itemx)"), // the car jack gameobject instance.
-                parent,
-                trigger,
-                new Vector3(-0.447f, 0, -1.48f), // Install position
-                Quaternion.Euler(0, 285, 0));// Install rotation
-            ModConsole.Print(string.Format("{0} v{1}: Loaded.", this.Name, this.Version));
+            GameObject satsuma = GameObject.Find("SATSUMA(557kg, 248)");
+            GameObject carJackGo = GameObject.Find("car jack(itemx)");
+
+            foldFsm = carJackGo.GetPlayMaker("Fold");
+
+            Trigger trigger = new Trigger("carJackTrigger", satsuma, new Vector3(-0.3770001f, 0f, -1.42f), new Vector3(0, 285, 0), new Vector3(0.2f, 0.2f, 0.24f));
+            AssemblyTypeJointSettings atjs = new AssemblyTypeJointSettings(satsuma.GetComponent<Rigidbody>());
+            PartSettings partSettings = new PartSettings() { assembleType = AssembleType.joint, assemblyTypeJointSettings = atjs, setPositionRotationOnInitialisePart = false, installedPartToLayer = TommoJProductions.ModApi.LayerMasksEnum.DontCollide };
+            carJackPart = carJackGo.AddComponent<Part>();
+            carJackPart.defaultSaveInfo = new PartSaveInfo() { installed = true };
+            carJackPart.onAssemble += CarJackPart_onAssemble;
+            carJackPart.onDisassemble += CarJackPart_onDisassemble;
+            carJackPart.initPart(loadData(), partSettings, trigger);
+            ModConsole.Print(string.Format("{0} v{1}: Loaded.", Name, Version));
         }
+
+        private void CarJackPart_onDisassemble()
+        {
+            foldFsm.enabled = true;
+        }
+
+        private void CarJackPart_onAssemble()
+        {
+            if (foldFsm.FsmVariables.BoolVariables[0].Value)
+            {
+                carJackPart.disassemble();
+                return;
+            }
+            foldFsm.enabled = false;
+        }
+
         /// <summary>
         /// Occurs on game save. (player at toilet)
         /// </summary>
@@ -69,11 +67,30 @@ namespace SecureCarJack
 
             try
             {
-                SaveLoad.SerializeSaveFile(this, this.carJackPart.getSaveInfo(), fileName);
+                SaveLoad.SerializeSaveFile(this, carJackPart.getSaveInfo(), FILE_NAME);
             }
             catch (System.Exception ex)
             {
                 ModConsole.Error("<b>[SecureCarJackMod]</b> - an error occured while attempting to save part info.. see: " + ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Loads save data.
+        /// </summary>
+        private PartSaveInfo loadData()
+        {
+            // Written, 12.10.2018
+
+            try
+            {
+                return SaveLoad.DeserializeSaveFile<PartSaveInfo>(this, FILE_NAME);
+            }
+            catch (System.NullReferenceException)
+            {
+                // no save file exists.. // setting/loading default save data.
+
+                return null;
             }
         }
     }
